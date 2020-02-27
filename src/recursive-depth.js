5559237c3b16860303f078ac6596e0d0ba38a084
Vladislav Shulyak <vladislav.dll@gmail.com>
2020-02-27 21:02:43 +0300

    feat: add some solutions

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,16 @@
+let test = [1];
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr, value = 2) {
+
+        for (let i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                this.calculateDepth(arr[i], value + 1);
+                test.push(value);
+            }
+        }
+        let temp =  Math.max(...test);
+        return temp;
+        test = [1];
+
     }
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,19 +1,30 @@
-// const chainMaker = {
-//     getLength() {
-//
-//     },
-//     addLink(value) {
-//
-//     },
-//     removeLink(position) {
-//
-//     },
-//     reverseChain() {
-//
-//     },
-//     finishChain() {
-//
-//     }
-// };
-//
-// module.exports = chainMaker;
+const chainMaker = {
+    chain: [],
+    getLength() {
+        this.chain.length;
+        return this;
+    },
+    addLink(value) {
+        this.chain.push(`( ${value} )`);
+        return this;
+    },
+    removeLink(position) {
+        if (position < this.chain.length && position > 0 && typeof (position) == 'number') {
+            this.chain.splice(position - 1, 1);
+            return this;
+        }
+        this.chain = [];
+        throw new Error();
+    },
+    reverseChain() {
+        this.chain.reverse();
+        return this;
+    },
+    finishChain() {
+        let array = this.chain;
+        this.chain = [];
+        return array.join('~~');
+    }
+};
+
+module.exports = chainMaker;

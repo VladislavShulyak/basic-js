e30274084b5456dfbf68e7aadfc6a80b031e32cf
Vladislav Shulyak <vladislav.dll@gmail.com>
2020-03-22 17:27:12 +0300

    feat: solve repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,24 +1,38 @@
 module.exports = function repeater(str, options) {
 
-        let string = '';
 
-
+    if (options.separator === undefined) {
+        options.separator = '+';
+    }
+    if (options.addition === undefined) {
+        options.addition = '';
+    }
+    if (options.additionRepeatTimes === undefined) {
+        options.additionRepeatTimes = 0;
+    }
+    if (options.additionSeparator === undefined) {
+        options.additionSeparator = '';
+    }
+    if (options.repeatTimes === undefined) {
+        return (str + options.addition)
+    } else {
+        let string;
         let stringSeparator = options.addition + options.additionSeparator;
-        let stringTemp ='';
+        let stringTemp = '';
         for (let i = 0; i < options.additionRepeatTimes; i++) {
-            stringTemp +=stringSeparator;
+            stringTemp += stringSeparator;
         }
         string = str + stringTemp;
-        string = string.substring(0,string.length - options.additionSeparator.length );
-
+        string = string.substring(0, string.length - options.additionSeparator.length);
 
         string = string + options.separator;
-        let stringTemp2 = string;
+        let stringSeparatorTemp = string;
         for (let i = 0; i < options.repeatTimes - 1; i++) {
-            string += stringTemp2;
+            string += stringSeparatorTemp;
         }
-        string = string.substring(0,string.length - options.separator.length );
+        string = string.substring(0, string.length - options.separator.length);
         return string;
+    }
 
 };
   

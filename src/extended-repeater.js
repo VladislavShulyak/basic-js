c173f888f2592f74f34f059f146b83ccbebcf0cb
Vladislav Shulyak <vladislav.dll@gmail.com>
2020-03-22 16:46:33 +0300

    feat: solve some test

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,24 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+        let string = '';
+
+
+        let stringSeparator = options.addition + options.additionSeparator;
+        let stringTemp ='';
+        for (let i = 0; i < options.additionRepeatTimes; i++) {
+            stringTemp +=stringSeparator;
+        }
+        string = str + stringTemp;
+        string = string.substring(0,string.length - options.additionSeparator.length );
+
+
+        string = string + options.separator;
+        let stringTemp2 = string;
+        for (let i = 0; i < options.repeatTimes - 1; i++) {
+            string += stringTemp2;
+        }
+        string = string.substring(0,string.length - options.separator.length );
+        return string;
+
 };
   
